4641a6abeba4848dcfcf82ca0b81be9edea19667
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-15 16:07:59 +0530

    [mahafuz] adding UC_whileLoop

---
+++ WhileLoop.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class WhileLoop
+    {
+        public static void WithWhileLoop()
+        {
+            const int FULL_TIME = 1;
+            const int PART_TIME = 2;
+            int totalWage = 0;
+            int empHrs = 0;
+            int empWage;
+            const int EMP_RATE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
+            int days = 1;
+            int empWorkingHrs = 0;
+
+
+
+            Random random = new Random();
+            while (days <= MAX_WORKING_DAYS && empWorkingHrs <= MAX_WORKING_HRS)
+            {
+
+
+                int randomInput = random.Next(0, 3);
+
+                switch (randomInput)
+                {
+                    case FULL_TIME:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is present fulltime");
+                        break;
+                    case PART_TIME:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is present parttime");
+                        break;
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is absent");
+                        break;
+                }
+                empWage = EMP_RATE_PER_HR * empHrs;
+                Console.WriteLine("EMployee wage for DAy {0} is {1}", days, empWage);
+                totalWage += empWage;
+                empWorkingHrs += empHrs;
+                days++;
+
+            }
+            Console.WriteLine("total wage for {0}Days and hrs:{1} is:{2} ", MAX_WORKING_DAYS, empWorkingHrs, totalWage);
+        }
+    }
+}
+
